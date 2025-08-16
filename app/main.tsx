9ecc3a12198876ff9575a7d3ab98e9f977686c13
Aekkasit Aisuwan <aekkasitrun@gmail.com>
2025-08-16 17:40:06 +0700

    rename

--- app/main.tsx
+++ app/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/AppLayout';
+import AppLayout from './components/AppLayout';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppLayout />
     </BrowserRouter>
   </React.StrictMode>
 );
